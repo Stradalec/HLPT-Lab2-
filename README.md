a5d41c67ef4ce00713d5ace9752702f5095399ca
Stradalec <pochtatipo6@gmail.com>
2025-10-08 20:54:37 +0700

    refactor: try to make pass Sonar test check

--- README.md
+++ README.md
@@ -10,5 +10,5 @@ cd C:/Путь/До/Папки/С этой/Программой
 Также можно запустить файл с тестами:
 ./test.sh
 В связи с тем, что Sonar за отсутствие тестов бьёт палками, добавляю сюда команды для создания тестов:
-kotlinc app.kt tests.kt -classpath "libs/junit-platform-console-standalone.jar;libs/kotlinx-cli-jvm-0.3.6.jar" -d test.jar
+kotlinc app.kt -classpath "libs/junit-platform-console-standalone.jar;libs/kotlinx-cli-jvm-0.3.6.jar" -d test.jar
 java -jar libs/junit-platform-console-standalone.jar --class-path test.jar;libs/kotlin-stdlib.jar --scan-class-path
--- app.kt
+++ app.kt
@@ -3,7 +3,8 @@ import kotlinx.cli.*
 import java.security.MessageDigest
 import java.nio.charset.StandardCharsets
 import kotlin.system.exitProcess
-
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 
 enum class ExitCode(val code: Int) {
@@ -174,3 +175,35 @@ class PermissionManager {
     }
 }
 
+class ResourceTest {
+
+  @Test
+  fun `Child work`() {
+    val parent = Resource("parent")
+    val child = Resource("child")
+    parent.addChild(child)
+    
+    val retrieved = parent.getChild("child")
+    assertNotNull(retrieved)
+    assertEquals(child, retrieved)
+  }
+
+  @Test
+  fun `findByPath check existing paths`() {
+    val root = Resource("root")
+    val child1 = Resource("child1")
+    val child2 = Resource("child2")
+    root.addChild(child1)
+    child1.addChild(child2)
+
+    val found = root.findByPath("child1.child2")
+    assertNotNull(found)
+    assertEquals(child2, found)
+  }
+
+  @Test
+  fun `findByPath check not existing path`() {
+    val root = Resource("root")
+    assertNull(root.findByPath("unknown.path"))
+  }
+}
